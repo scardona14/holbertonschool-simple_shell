c22c044947a195a24229ef03d85c5d68709ba9ee
LNCarrion <lestycarrion@gmail.com>
2023-12-18 13:06:14 -0400

    fix: deleting new line from childprocess

--- shell.c
+++ shell.c
@@ -72,9 +72,9 @@ void execute_command(TokenList *tokens)
 
 		waitpid(pid, &status, 0);
 		if (WIFEXITED(status))
-			printf("Child process exited with status %d\n", WEXITSTATUS(status));
+			printf("Child process exited with status %d", WEXITSTATUS(status));
 		else
-			printf("child process did not exit normally\n");
+			printf("child process did not exit normally");
 	}
 }
 
